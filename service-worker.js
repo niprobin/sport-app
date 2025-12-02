0d9c727049ea247865f38ccb895bebcf23971cab
niprobin <niprobin@gmail.com>
2025-12-02 14:42:15 +0100

    PWA

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+﻿const CACHE_NAME = 'renfo-sport-app-v1';
+const OFFLINE_RESOURCES = [
+  './',
+  './index.html',
+  './renfo-foot.json',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(OFFLINE_RESOURCES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  const request = event.request;
+  const url = new URL(request.url);
+
+  if (url.origin === self.location.origin) {
+    event.respondWith(
+      caches.match(request).then((cached) => {
+        if (cached) return cached;
+        return fetch(request)
+          .then((response) => {
+            const clone = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(request, clone));
+            return response;
+          })
+          .catch(() => caches.match('./index.html'));
+      })
+    );
+    return;
+  }
+
+  event.respondWith(
+    fetch(request).catch(() => caches.match('./index.html'))
+  );
+});
